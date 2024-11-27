80cb28a68da25f5fda8f0359a22a1e8e3822bbf1
Lucy-Lu-mymsd <lulucy2015@hotmail.com>
2024-11-28 10:39:15 +1300

    test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	// Test1
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
